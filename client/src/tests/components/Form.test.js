11edb6386aaa6cdd0df382dc1771f501a9840afb
mdashmiller <m.robert.miller@gmail.com>
2019-03-18 11:32:56 -0500

    add useState to Form component

--- client/src/tests/components/Form.test.js
+++ client/src/tests/components/Form.test.js
@@ -18,27 +18,126 @@ describe('Form rendering', () => {
     component = setUp()
   })
 
-  it('should render 1 form', () => {
-    const wrapper = findByTestAttr(component, 'contact-form')
-    expect(wrapper.length).toBe(1)
+  describe('when a submission has not been made', () => {
+
+    it('should render 1 form', () => {
+      const wrapper = findByTestAttr(component, 'contact-form')
+      expect(wrapper.length).toBe(1)
+    })
+
+    it('should render 1 name input', () => {
+      const wrapper = findByTestAttr(component, 'name')
+      expect(wrapper.length).toBe(1)
+    })
+
+    it('should render 1 message input', () => {
+      const wrapper = findByTestAttr(component, 'message')
+      expect(wrapper.length).toBe(1)
+    })
+
+    it('should render 1 submit input', () => {
+      const wrapper = findByTestAttr(component, 'submit')
+      expect(wrapper.length).toBe(1)
+    })
+
   })
 
-  it('should render 1 name input', () => {
-    const wrapper = findByTestAttr(component, 'name')
-    expect(wrapper.length).toBe(1)
-  })
+  // // can't test these conditional renders until
+  // // Enzyme adds support for Hooks
+  // describe('after a submission event', () => {
 
-  it('should render 1 message input', () => {
-    const wrapper = findByTestAttr(component, 'message')
-    expect(wrapper.length).toBe(1)
-  })
+  //   it('should render 1 form', () => {
+  //     const wrapper = findByTestAttr(component, 'contact-form')
+  //     expect(wrapper.length).toBe(1)
+  //   })
 
-  it('should render 1 submit input', () => {
-    const wrapper = findByTestAttr(component, 'submit')
-    expect(wrapper.length).toBe(1)
-  })
+  //   it('should render 1 name input', () => {
+  //     const wrapper = findByTestAttr(component, 'name')
+  //     expect(wrapper.length).toBe(1)
+  //   })
+
+  //   it('should render 1 message input', () => {
+  //     const wrapper = findByTestAttr(component, 'message')
+  //     expect(wrapper.length).toBe(1)
+  //   })
+
+  //   it('should render two output paragraphs', () => {
+
+  //   })
+
+  //   it('should render 1 clear button', () => {
+  //     const wrapper = findByTestAttr(component, 'clear')
+  //     expect(wrapper.length).toBe(1)
+  //   })
+
+  // })
 
 })
+
+// // can't test these functions until Enzyme adds
+// // support for Hooks
+// describe('setName()', () => {
+  
+//   const event = {
+//     target: {
+//       value: 'some string'
+//     }
+//   }
+
+//   let component, instance
+//   beforeEach(() => {
+//     component = setUp()
+//     instance = component.instance()
+//   })
+
+//   describe('spying on setName()', () => {
+
+//     it('should be called when user types in the name field', () => {
+//       const wrapper = findByTestAttr(component, 'name') 
+//       jest.spyOn(instance, 'setName')
+
+//       wrapper.simulate('change', event)
+
+//       expect(instance.setName).toHaveBeenCalledTimes(2)
+//     })
+
+//   })
+
+//   describe('directly invoking on setName()', () => {
+
+//     it('should set the name in state', () => {
+
+//     })
+
+//   })
+
+// })
+
+// describe('setMessage()', () => {
+
+//   let component, instance
+//   beforeEach(() => {
+//     component = setUp()
+//     instance = component.instance()
+//   })
+
+//   describe('spying on setMessage()', () => {
+
+//     it('should be called when user types in the message field', () => {
+
+//     })
+
+//   })
+
+//   describe('directly invoking on setMessage()', () => {
+
+//     it('should set the message in state', () => {
+
+//     })
+
+//   })
+
+// })
 
 describe('Form mounting and unmounting', () => {
 
